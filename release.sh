f535c0a2d65a81129a269b01a334c4510c2ee69a
jilen <jilen.zhang@gmail.com>
2019-10-21 11:32:09 +0800

    try fix

--- release.sh
+++ release.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -e # Any subsequent(*) commands which fail will cause the shell script to exit immediately
 
-sbt ++2.11.12 -Dquill.macro.log=false -Dquill.scala.version=2.11.12 release
-sbt ++2.12.6 -Dquill.macro.log=false -Dquill.scala.version=2.12.6 release
 sbt ++2.13.1 -Dquill.macro.log=false -Dquill.scala.version=2.13.1 release
+sbt ++2.12.10 -Dquill.macro.log=false -Dquill.scala.version=2.12.10 release
+sbt ++2.11.12 -Dquill.macro.log=false -Dquill.scala.version=2.11.12 release
